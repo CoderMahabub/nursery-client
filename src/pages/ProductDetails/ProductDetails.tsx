721b4ee22677ea0f3961cb7a7e3003a7eafccc6f
CoderMahabub <CoderMahabub@gmail.com>
2024-07-28 02:08:35 +0900

    Pages Create & Routes Setup

---
+++ src/pages/ProductDetails/ProductDetails.tsx
@@ -0,0 +1,9 @@
+const ProductDetails = () => {
+  return (
+    <div>
+      <h1>ProductDetails Component</h1>
+    </div>
+  );
+};
+
+export default ProductDetails;
--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,6 +4,10 @@ import Home from "../pages/Home/Home";
 import ErrorPage from "../pages/ErrorPage/ErrorPage";
 import Products from "../pages/Products/Products";
 import Dashboard from "../pages/Dashboard/Dashboard";
+import ProductDetails from "../pages/ProductDetails/ProductDetails";
+import Cart from "../pages/Cart/Cart";
+import Checkout from "../pages/Checkout/Checkout";
+import Payment from "../pages/Payment/Payment";
 
 const router = createBrowserRouter([
   {
@@ -22,6 +26,22 @@ const router = createBrowserRouter([
       {
         path: "/dashboard",
         element: <Dashboard></Dashboard>,
+      },
+      {
+        path: "/product/:id",
+        element: <ProductDetails></ProductDetails>,
+      },
+      {
+        path: "/cart",
+        element: <Cart></Cart>,
+      },
+      {
+        path: "/checkout",
+        element: <Checkout></Checkout>,
+      },
+      {
+        path: "/payment",
+        element: <Payment></Payment>,
       },
     ],
   },
